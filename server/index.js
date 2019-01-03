a82f706bcd49364b4bb055c1c5f19404479458f0
Kushan Singh <kushansingh22@gmail.com>
2019-01-03 12:39:22 +0530

    Authentication Phase 1

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Keys = require('./config/keys');
 const app = new express();
 
 
-app.get('/',(req,res) => {
-    res.send({bye: 'buddy'});
-});
+passport.use(new GoogleStrategy({
+    clientID: Keys.googleClientID,
+    clientSecret: Keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+},(accessToken,refreshToken,profile,done) => {
+    console.log('Access Token',accessToken);
+    console.log('Refresh Token',refreshToken);
+    console.log('Profile',profile);
+}));  // the strategy to use we have to provide client id and client secret
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.get('/auth/google', passport.authenticate('google',{
+    scope: ['profile','email']
+})
+
+);
+
+app.get('/auth/google/callback',passport.authenticate('google'));
+ 
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT); 
+
