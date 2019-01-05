9778c127474d287224e005396d9340258c02b56c
Kushan Singh <kushansingh22@gmail.com>
2019-01-05 14:45:35 +0530

    Authentication server side done

--- server/index.js
+++ server/index.js
@@ -1,27 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const Keys = require('./config/keys');
+const keys = require('./config/keys');
+
+require('./models/user');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
+
 const app = new express();
 
-
-passport.use(new GoogleStrategy({
-    clientID: Keys.googleClientID,
-    clientSecret: Keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-},(accessToken,refreshToken,profile,done) => {
-    console.log('Access Token',accessToken);
-    console.log('Refresh Token',refreshToken);
-    console.log('Profile',profile);
-}));  // the strategy to use we have to provide client id and client secret
-
-app.get('/auth/google', passport.authenticate('google',{
-    scope: ['profile','email']
-})
-
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
 );
 
-app.get('/auth/google/callback',passport.authenticate('google'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+
+
  
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT); 
---
+++ server/services/passport.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const Keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user,done) => {
+    done(null,user.id);
+});
+
+passport.deserializeUser((id,done) => {
+    User.findById(id)
+        .then((user) => {
+            done(null,user);
+        })
+});
+
+passport.use(new GoogleStrategy({
+    clientID: Keys.googleClientID,
+    clientSecret: Keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+},
+
+(accessToken,refreshToken,profile,done) => {
+
+    User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+            if (existingUser){
+                // We already have a record    
+                console.log("User Exists");
+                done(null,existingUser);
+            }else{
+                new User({ googleId: profile.id }).save()
+                    .then((user) => {
+                        done(null,user);
+                    });
+            }
+        })
+
+    
+})
+
+);  // the strategy to use we have to provide client id and client secret
