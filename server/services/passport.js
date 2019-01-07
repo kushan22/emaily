4b6c6996dc8fd424d02c85a8e1a83ac6297cdf25
Kushan Singh <kushansingh22@gmail.com>
2019-01-07 12:38:08 +0530

    Authentiation flow done

--- server/services/passport.js
+++ server/services/passport.js
@@ -19,7 +19,8 @@ passport.deserializeUser((id,done) => {
 passport.use(new GoogleStrategy({
     clientID: Keys.googleClientID,
     clientSecret: Keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 },
 
 (accessToken,refreshToken,profile,done) => {
